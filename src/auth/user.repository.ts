3a074e055ec0cc53074da28436260b1923dd840d
Maverick Tesini <Maverick.Tesini@octoconsulting.com>
2020-01-09 11:35:07 -0500

    develop rudimentary auth functionality for user signup and apply a validation pipe

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,5 +1,16 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentailsDto } from './dto/auth-credentials.dto';
 
 @EntityRepository(User)
-export class UserRepository extends Repository<User> {}
+export class UserRepository extends Repository<User> {
+  async signUp(authCredentailsDto: AuthCredentailsDto): Promise<void> {
+    const { username, password } = authCredentailsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+
+    await user.save();
+  }
+}
