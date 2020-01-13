e2a38fe3b5c94118156efe870c3b94dfff048ec0
Maverick Tesini <Maverick.Tesini@octoconsulting.com>
2020-01-13 11:33:35 -0500

    configure aws cors in production.yml and main server app

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ import * as config from 'config';
 async function bootstrap() {
   const logger = new Logger('boostrap');
   const app = await NestFactory.create(AppModule);
-
-  if (process.env.NODE_ENV === 'development') {
-    app.enableCors();
-  }
-
   const serverConfig = config.get('server');
+
+  if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'dev') {
+    logger.log('Dev mode detected');
+    app.enableCors();
+  } else {
+    app.enableCors({ origin: serverConfig.origin });
+  }
 
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
